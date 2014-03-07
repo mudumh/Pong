542bd1079e7a48fb253a93198297889c865bc0a8
mudumh <mvharsha.mail@gmail.com>
2014-03-07 09:58:42 -0700

    ready to push git

--- pong.js
+++ pong.js
@@ -1,15 +1,28 @@
-function Background(){
+function Background() {}
 
+Background.prototype.draw = function(context) {
+  context.fillStyle = '#000'
+  context.fillRect(0, 0, game.width, game.height)
 
+  // Print scores
+  context.fillStyle = '#fff'
+  context.font = "40px monospace"
+  context.fillText(game.player.score, game.width * 3 / 8, 50)
+  context.fillText(game.bot.score,    game.width * 5 / 8, 50)
 }
 
-Background.prototype.draw  = function(context){
-	context.fillStyle = "#000";
-	context.fillRect(0,0,game.width,game.height);
-}
 
-var canvas = $("canvas")[0];
-var game = new Game(canvas);
-game.entities = [new Background()];
+// Initialize and start the game
+
+var game = new Game($('canvas')[0])
+
+// Load the game entities
+game.entities = [
+  new Background(),
+  game.ball = new Ball(),
+  game.player = new Player(),
+  game.bot = new Bot()
+]
+
 game.start();
-canvas.focus();
+$('canvas')[0].focus();
